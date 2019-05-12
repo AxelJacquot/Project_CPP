a4e92f20dd41d3d453fb1a8853cb6f4026f42903
Axel Jacquot <axel.jacquot@outlook.com>
2019-05-12 19:21:04 +0200

    Update

---
+++ Serial_CPP/mainwindow.h
@@ -0,0 +1,35 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QComboBox>
+#include <QPushButton>
+#include <QtSerialPort/QSerialPort>
+#include <QDebug>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    void init_port();
+    ~MainWindow();
+
+
+
+public slots:
+    void transmitCombo(int);
+    void transmitCAN(void);
+    void readData();
+
+private:
+    Ui::MainWindow *ui;
+    QSerialPort* m_serial;
+};
+
+#endif // MAINWINDOW_H
